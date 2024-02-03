4ed0c53f6edc083f557240644e4a9eefa50ee6d6
XYL <1767040019@qq.com>
2024-02-04 00:47:06 +0800

    change the settings,seperate it into three part

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings')
+    profile = os.environ.get('TYPEIDEA_PROFILE','develop')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings.%s' % profile)
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
