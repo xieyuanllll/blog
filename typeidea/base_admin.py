30569fabc2a0f0809d099d41c4526ff85b4581ce
XYL <1767040019@qq.com>
2024-02-06 17:05:33 +0800

    第七章 136页

---
+++ typeidea/base_admin.py
@@ -0,0 +1,16 @@
+from typing import Any
+from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+
+class BaseOwnerAdmin(admin.ModelAdmin):
+
+    exclude = ('owner',)
+
+    def get_queryset(self, request: HttpRequest):
+        qs = super(BaseOwnerAdmin,self).get_queryset(request)
+        return qs.filter(owner=request.user)
+    
+    def save_model(self, request: Any, obj: Any, form: Any, change: Any):
+        obj.owner = request.user
+        return super(BaseOwnerAdmin,self).save_model(request, obj, form, change)
